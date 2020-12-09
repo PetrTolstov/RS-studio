b485237dde64bf564a3878940e48237d6649b36a
PetrTolstov <57655896+PetrTolstov@users.noreply.github.com>
2020-12-09 11:49:11 +0200

    comment+ 09.12

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,24 @@
 var express = require('express');
 var router = express.Router();
+//mongodb+srv://main_admin:7kvxfrqBKl899TJQ@main.rhunr.mongodb.net/SR-studio?retryWrites=true&w=majority
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://main_admin:7kvxfrqBKl899TJQ@main.rhunr.mongodb.net/SR-studio?retryWrites=true&w=majority&useUnifiedTopology=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("main").collection("sample_analytics");
+
+  let user = {name: "Tom", sex:'none', age: 23};
+  collection.insertOne(user, function(err, result){
+
+    if(err){
+      return console.log(err);
+    }
+    console.log(result.ops);
+    client.close();
+  });
+
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
