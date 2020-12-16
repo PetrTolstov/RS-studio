2c9dc82c728c89bc928f139ab71297a62b4f1926
PetrTolstov <57655896+PetrTolstov@users.noreply.github.com>
2020-12-16 19:38:03 +0200

    new dawn

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 //mongodb+srv://main_admin:7kvxfrqBKl899TJQ@main.rhunr.mongodb.net/SR-studio?retryWrites=true&w=majority
-
+/* GET home page.
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://main_admin:7kvxfrqBKl899TJQ@main.rhunr.mongodb.net/SR-studio?retryWrites=true&w=majority&useUnifiedTopology=true";
 const client = new MongoClient(uri, { useNewUrlParser: true });
@@ -19,9 +19,12 @@ client.connect(err => {
   });
 
 });
+
+
+ */
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 module.exports = router;
